88850ce841fd6aa5eb576f2a4aa1c72185913ba0
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-10-22 02:42:13 +0100

    Add: Updated the contact model to include fields.

--- api/contacts/models.py
+++ api/contacts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ..authentication.models import MyCustomUser
 
-# Create your models here.
+
+class Contact(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        MyCustomUser,
+        on_delete=models.CASCADE,
+        related_name='my_custom_user_contacts',
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.phone_number}'
