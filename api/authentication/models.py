888352a8de60fb644686fe8b5e1fec36c8ee1003
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-10-22 02:34:09 +0100

    Feat: Included character fields to authentication Model

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.hashers import make_password, check_password
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class MyCustomUser(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    confirm_password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    groups = models.ManyToManyField(Group, related_name='my_custom_user_groups', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='my_custom_user_permissions', blank=True)
+
+    def set_password(self, password):
+        self.password = make_password(password)
+
+    def check_password(self, password):
+        return check_password(password, self.password)
+
+    def generate_auth_token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh)
+        }
+
+    def __str__(self):
+        return f'<User {self.email}>'
+
